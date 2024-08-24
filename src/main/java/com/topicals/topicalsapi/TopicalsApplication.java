1ce1dd69a681ccff23264fb1687b534bb61ad208
Bradley Mwangangi <177125727+aceof10@users.noreply.github.com>
2024-08-24 23:10:46 +0300

    initial commit

---
+++ src/main/java/com/topicals/topicalsapi/TopicalsApplication.java
@@ -0,0 +1,13 @@
+package com.topicals.topicalsapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TopicalsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TopicalsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/topicals/topicalsapi/TopicalsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.topicals.topicalsapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TopicalsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
